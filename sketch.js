575156f70ee52ff2d5133fa1a3cd6c4052e51b86
maria paula <mp.vargast79@gmail.com>
2019-11-19 23:17:20 +0100

    upload

--- sketch.js
+++ sketch.js
@@ -1,11 +1,55 @@
-function preload(){
-  // put preload code here
+
+var value = 0;
+var mtch;
+
+function preload() {
+  mtch = loadImage("./assets/matches.png");
 }
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+  angleMode(DEGREES);
+  noStroke();
+  frameRate(10);
+  setShakeThreshold(30);
+
 }
 
 function draw() {
-  // put drawing code here
+
+  fill(255, 190, 0, value);
+  ellipse((width / 2) - 3, (height / 2) - 55, 60, 100);
+
+  fill(255, 120, 0, value);
+  ellipse((width / 2) - 3, (height / 2) - 80, 90, 180);
+
+  image(mtch, (width / 2) - 15, (height / 2) - 50, mtch.width / 2, mtch.height / 2);
+
+  var call = "shake me";
+  drawingContext.font = "normal 15px Alata";
+  drawingContext.textAlign = "center";
+  fill(0);
+  text(call, (width / 2) - 15, (height / 2) - 220);
+
+  var call2 = "to light me up";
+  drawingContext.font = "normal 15px Alata";
+  drawingContext.textAlign = "center";
+  fill(0);
+  text(call2, (width / 2) - 15, (height / 2) - 195);
+}
+
+
+function deviceShaken() {
+  value++;
+  if (value > 255) {
+    value = 0;
+  }
+}
+
+function touchMoved() {
+  return false;
+}
+
+function touchEnded() {
+  DeviceOrientationEvent.requestPermission();
 }
